ca9a8ef39261559d0b8eafa1ab678900128cc7f9
Yukai Huang <yukaihuangtw@gmail.com>
2016-07-10 14:55:34 +0800

    install node-uuid and redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,17 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import reducer from 'reducers';
+import createLogger from 'redux-logger';
+
+const logger = createLogger();
 
 export default function configureStore(initialState) {
 	const sagaMiddleware = createSagaMiddleware();
 
 	const createStoreWithMiddleware = applyMiddleware(
 		sagaMiddleware,
-		thunk
+		thunk,
+		logger
 	)(createStore);
 
 	const store = createStoreWithMiddleware(reducer, initialState);
