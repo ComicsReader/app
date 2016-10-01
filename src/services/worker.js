84fb9510e0155b6dd407ba9c7913709ac13d5017
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-01 22:41:24 +0800

    Return update chapters by checking chapter cache

--- src/services/worker.js
+++ src/services/worker.js
@@ -2,23 +2,58 @@ import { firebaseApp } from 'utils/firebase';
 import { comicManagers } from 'services';
 
 function getUnreadChapters({comicID, deviceID}) {
-	const ReadingRecord = firebaseApp.database().ref(`users/${deviceID}/readingRecord/`);
+	const ReadingRecord = firebaseApp.database().ref(`users/${deviceID}/readingRecord/${comicID}`);
+	const ChapterCache = firebaseApp.database().ref(`users/${deviceID}/chapterCache/${comicID}`);
+
 	return new Promise((resolve) => {
 		comicManagers.dm5.getComic(comicID).then(comicInfo => {
 			const { chapters } = comicInfo;
 
 			ReadingRecord.once('value').then(snapeshot => {
-				if (snapeshot && snapeshot.val()) {
-					let readingRecord = snapeshot.val()[comicID];
+				let readingRecord = snapeshot.val();
 
-					if (typeof readingRecord === 'undefined') {
-						resolve(chapters);
+				const notificationSent = (chapterItem) => {
+					return readingRecord && readingRecord[chapterItem.chapterID] && readingRecord[chapterItem.chapterID] === 'notification_sent';
+				};
+
+				ChapterCache.once('value').then(snapeshot => {
+					let chapterCache = snapeshot.val();
+
+					if (typeof chapterCache === 'undefined' || !chapterCache) {
+						if (typeof readingRecord === 'undefined' || !readingRecord) {
+							// haven't read it yet, just report the latest chapter
+							resolve(chapters.slice(0, 1));
+						} else {
+							// no chapterCache yet, my happen on upgrade version
+							resolve(chapters.slice(0, 1).filter(notificationSent));
+						}
 					} else {
-						resolve(chapters.filter(chapter => typeof readingRecord[chapter.chapterID] === 'undefined' || !readingRecord[chapter.chapterID]));
+						if (typeof readingRecord === 'undefined' || !readingRecord) {
+							// haven't read it yet, just report the latest chapter
+							resolve(chapters.slice(0, 1));
+						} else {
+							if (chapters.length > chapterCache.length) {
+								// has update chapters! Let's report them
+								let index = chapters.findIndex(chapter => chapter.chapterID === chapterCache[0].chapterID);
+
+								if (index != -1 && index > 0) {
+									resolve(chapters.slice(0, index).filter(notificationSent));
+								} else {
+									// some problem, still report latest chapter
+									resolve(chapters.slice(0, 1).filter(notificationSent));
+								}
+							} else {
+								if (chapters[0].chapterID === chapterCache[0].chapterID) {
+									// no updates!
+									resolve([]);
+								} else {
+									// some problem, still report latest chapter
+									resolve(chapters.slice(0, 1).filter(notificationSent));
+								}
+							}
+						}
 					}
-				} else {
-					resolve(chapters);
-				}
+				});
 			});
 		});
 	});
