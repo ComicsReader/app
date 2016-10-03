2cc2c7d6377a79d439a0ef582f65bc653520ac78
Yukai Huang <yukaihuangtw@gmail.com>
2016-10-03 12:19:14 +0800

    Revert f3f8d6b electron webpack config

--- baseConfig.js
+++ baseConfig.js
@@ -2,36 +2,47 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	module:{
-		loaders: [
-			{
-				test: /\.json$/,
-				loader: 'json-loader'
-			},{
-				test: /\.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: { compact: false }
-			},{
-				test: /\.less$/,
-				loader: ExtractTextPlugin.extract('style-loader','css-loader!less-loader')
-			},{
-				test: /\.css$/,
-				loader: ExtractTextPlugin.extract('style-loader','css-loader')
-			},
-			{
-				test: /\.scss$/,
-				loaders: ['style', 'css', 'sass']
-			},
-			{
-				test: /\.(png|jpg|gif)$/,
-				loader: 'url-loader?limit=8192'
-			},
-			{ test: /\.(ttf|eot|svg)$/,
-				loader: 'url-loader?limit=100000'
+	module: {
+		loaders: [{
+			test: /\.json$/,
+			loader: 'json-loader'
+		}, {
+			test: /\.jsx?$/,
+			loader: 'babel-loader',
+			exclude: /node_modules/,
+			query: {
+				compact: false
 			}
-		]
+		}, {
+			test: /\.less$/,
+			loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+		}, {
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+		}, {
+			test: /\.scss$/,
+			loaders: ['style', 'css', 'sass']
+		}, {
+			test: /\.(png|jpg|gif)$/,
+			loader: 'url-loader?limit=8192'
+		}, {
+			test: /\.(ttf|eot|svg)$/,
+			loader: 'url-loader?limit=100000'
+		}]
 	},
+	externals: [
+		(function () {
+			var IGNORES = [
+				'electron'
+			];
+			return function (context, request, callback) {
+				if (IGNORES.indexOf(request) >= 0) {
+					return callback(null, `require('${request}')`);
+				}
+				return callback();
+			};
+		})()
+	],
 	resolve: {
 		root: path.resolve('./src'),
 		extensions: ['', '.js']
