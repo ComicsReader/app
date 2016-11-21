dcd9e3c27a1c05ec9500f3a210347f35bb05dcc9
Yukai Huang <yukaihuangtw@gmail.com>
2016-11-21 12:09:59 +0800

    Replace firebase actions with pouchdb

--- src/actions/ConfigActions.js
+++ src/actions/ConfigActions.js
@@ -1,77 +1,126 @@
-import { firebaseApp } from 'utils/firebase';
+import PouchDB from 'pouchdb';
 import * as t from 'constants/ActionTypes';
 
-import store from 'store';
-
-let deviceID = store.get('device_id');
-
-export const resourceBaseUrl = `users/${deviceID}`;
-export const Collection = firebaseApp.database().ref(`${resourceBaseUrl}/collections/`);
-export const RecentComic = firebaseApp.database().ref(`${resourceBaseUrl}/recentComics/`);
-export const ReadingRecord = firebaseApp.database().ref(`${resourceBaseUrl}/readingRecord/`);
-export const ChapterCache = firebaseApp.database().ref(`${resourceBaseUrl}/chapterCache/`);
+export const Collection = new PouchDB({ name: 'collections' });
+export const RecentComic = new PouchDB({ name: 'recentComics' });
+export const ReadingRecord = new PouchDB({ name: 'readingRecord' });
+export const ChapterCache = new PouchDB({ name: 'chapterCache' });
 
 /* Collection Resource */
 export const fetchCollections = () => {
 	return dispatch => {
-		Collection.on('value', snapshot => {
+		Collection.allDocs({
+			include_docs: true
+		}).then(({rows: data}) => {
+			const collections = data.reduce((prev, cur) => {
+				return {...prev, [cur.doc.comicID]: cur.doc};
+			}, {}) || {};
 			dispatch({
 				type: t.FETCH_ALL_COLLECTION,
-				collections: snapshot.val() || {}
+				collections
 			});
 		});
 	};
 };
 
 export const turnOffFetchCollectionCallback = () => {
-	return dispatch => Collection.off();
+	// console.log('turnOffFetchCollectionCallback');
+	return dispatch => {
+	// 	CollectionChanges && CollectionChanges.cancel();
+	// 	CollectionChanges = null;
+	};
 };
 
 export const addCollection = (comic) => {
-	return dispatch => firebaseApp.database().ref(`${resourceBaseUrl}/collections/${comic.comicID}`).set({...comic, created_at: new Date().getTime()});
+	return dispatch => {
+		Collection.get(comic.comicID).catch(err => {
+			Collection.put({
+				_id: comic.comicID,
+				...comic,
+				created_at: new Date().getTime()
+			}).then(() => {
+				fetchCollections()(dispatch);
+			}).catch(err => {
+				console.log(`errr: ${err}`);
+			});
+		});
+	};
 };
 
 export const removeCollection = (key, callback=null) => {
 	return dispatch => {
-		Collection.child(key).remove();
-		if (callback) callback();
+		Collection.get(key).then(comic => {
+			Collection.remove(comic).then(() => {
+				fetchCollections()(dispatch);
+				if (callback) callback();
+			});
+		});
 	};
 };
 
 /* RecentComic Resource */
 export const fetchRecentComic = () => {
 	return dispatch => {
-		RecentComic.on('value', snapshot => {
+		RecentComic.allDocs({
+			include_docs: true
+		}).then(({rows: data}) => {
+			const recentComics = data.reduce((prev, cur) => {
+				return {...prev, [cur.doc.comicID]: cur.doc};
+			}, {}) || {};
 			dispatch({
 				type: t.FETCH_RECENT_COMICS,
-				recentComics: snapshot.val() || {}
+				recentComics
 			});
 		});
 	};
 };
 
 export const turnOffFetchRecentComicCallback = () => {
-	return dispatch => RecentComic.off();
+	return dispatch => {
+		// RecentComicChanges && RecentComicChanges.cancel();
+		// CollectionChanges = null;
+	};
 };
 
 export const addRecentComic = (comic) => {
-	return dispatch => firebaseApp.database().ref(`${resourceBaseUrl}/recentComics/${comic.comicID}`).set({...comic, last_read_at: new Date().getTime()});
+	return dispatch => {
+		RecentComic.get(comic.comicID).catch(err => {
+			RecentComic.put({
+				_id: comic.comicID,
+				...comic,
+				last_read_at: new Date().getTime()
+			}).then(() => {
+				fetchRecentComic()(dispatch);
+			}).catch(err => {
+				console.log(`errr: ${err}`);
+			});
+		});
+	};
 };
 
 export const removeRecentComic = (key, callback=null) => {
 	return dispatch => {
-		RecentComic.child(key).remove();
-		if (callback) callback();
+		RecentComic.get(key).then(comic => {
+			RecentComic.remove(comic).then(() => {
+				fetchRecentComic()(dispatch);
+				if (callback) callback();
+			});
+		});
 	};
 };
 
 /* Reading Resource */
 export const fetchReadingRecord = () => {
 	return dispatch => {
-		ReadingRecord.on('value', snapshot => {
+		ReadingRecord.allDocs({
+			include_docs: true
+		}).then(({rows: data}) => {
+			const readingRecord = data.reduce((prev, cur) => {
+				return {...prev, [cur.doc.comicID]: cur.doc};
+			}, {}) || {};
 			dispatch({
 				type: t.FETCH_READING_RECORD,
-				readingRecord: snapshot.val() || {}
+				readingRecord
 			});
 		});
 	};
@@ -80,10 +129,15 @@ export const fetchReadingRecord = () => {
 /* ChapterCache Resource */
 export const fetchChapterCache = () => {
 	return dispatch => {
-		ChapterCache.on('value', snapshot => {
+		ChapterCache.allDocs({
+			include_docs: true
+		}).then(({rows: data}) => {
+			const chapterCache = data.reduce((prev, cur) => {
+				return {...prev, [cur.doc.comicID]: cur.doc};
+			}, {}) || {};
 			dispatch({
 				type: t.FETCH_CHAPTER_CACHE,
-				chapterCache: snapshot.val() || {}
+				chapterCache
 			});
 		});
 	};
