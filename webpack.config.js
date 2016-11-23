65ee9a913f1358d93f8729c897da3fdfdc9f30cf
Yukai Huang <yukaihuangtw@gmail.com>
2016-11-23 15:44:00 +0800

    Refactor webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,43 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 
-const baseConfig = Object.assign({}, require('./webpackBaseConfig'), {
-	plugins: [
-		new ExtractTextPlugin('css/[name].css')
-	],
-	devtool: 'eval'
-});
+let baseConfig;
+
+if (process.env.NODE_ENV === 'development') {
+	baseConfig = Object.assign({}, require('./webpackBaseConfig'), {
+		plugins: [
+			new ExtractTextPlugin('css/[name].css')
+		],
+		devtool: 'eval'
+	});
+
+} else {
+	baseConfig = Object.assign({}, require('./webpackBaseConfig'), {
+		plugins: [
+			new ExtractTextPlugin('css/[name].css'),
+			new webpack.optimize.UglifyJsPlugin({
+				mangle: true,
+				sourcemap:false,
+				compress: {
+					sequences: true,
+					dead_code: true,
+					booleans: true,
+					drop_console: true,
+					properties: true,
+					loops:true,
+					if_return:true,
+					comparisons:true,
+					warnings:false
+				}
+			}),
+			new webpack.DefinePlugin({
+				'process.env': {
+					'NODE_ENV': JSON.stringify('production')
+				}
+			})
+		]
+	});
+}
 
 const vendorPackages = [
 	'react',
