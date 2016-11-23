a14133a9a3f4301cf0714b1d4fdbdfdc5cb24335
Yukai Huang <yukaihuangtw@gmail.com>
2016-11-23 15:31:35 +0800

    Config webpack code splitting vendor chunk

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,28 @@ const baseConfig = Object.assign({}, require('./webpackBaseConfig'), {
 	plugins: [
 		new ExtractTextPlugin('css/[name].css')
 	],
-	devtool: '#inline-source-map'
+	devtool: 'eval'
 });
 
+const vendorPackages = [
+	'react',
+	'react-dom',
+	'react-router',
+	'redux',
+	'material-ui',
+	'radium',
+	'pouchdb',
+	'lodash',
+	'cheerio',
+	'bluebird'
+];
+
 module.exports = [
+	/* Chrome extension build */
 	Object.assign({}, baseConfig, {
 		name: 'chrome',
 		entry: {
+			vendor: vendorPackages,
 			app:'./src/app.js',
 			background:'./src/platform/chrome-ext/background.js'
 		},
@@ -24,13 +39,16 @@ module.exports = [
 		plugins: [
 			new webpack.DefinePlugin({
 				PLATFORM: JSON.stringify('chrome')
-			})
+			}),
+			new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js')
 		]
 	}),
 
+	/* Electron build */
 	Object.assign({}, baseConfig, {
 		name: 'electron',
 		entry: {
+			vendor: vendorPackages,
 			app:'./src/app.js'
 		},
 		output: {
@@ -40,11 +58,13 @@ module.exports = [
 		plugins: [
 			new webpack.DefinePlugin({
 				PLATFORM: JSON.stringify('electron')
-			})
+			}),
+			new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js')
 		],
 		target: 'electron-renderer'
 	}),
 
+	/* Web Worker build */
 	Object.assign({}, baseConfig, {
 		name: 'worker',
 		entry: {
